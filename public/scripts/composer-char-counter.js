1df5def82f7f866c0d46779094fb93b82fc02355
Araxiah <cewoo98@hotmail.com>
2019-10-20 16:01:52 -0400

    Fixed the error where no error message would show up when you tried to submit nothing

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -11,7 +11,9 @@ $(document).ready(function () {
     if (num < 140) {
       counter.css("color", "black");
     }
-
+    if (num > 0) {
+      $(".emptytexterr").slideUp(1000);
+    }
     //Disables the button and call the error toggle if the char count is at -1 or below
     if (num === 141) {
       $(".texterr").slideToggle(1000);
@@ -23,4 +25,12 @@ $(document).ready(function () {
       $("#submitTweet input").prop('disabled', false);
     }
   });
+
+  //Brings down an error message when you try to submit nothing
+  $('.tweetOut').on('click', function () {
+    let num = $('.new-tweet form textarea').val().length;
+    if (num === 0) {
+      $('.emptytexterr').slideDown(1000)
+    }
+  })
 });
